3ecd8422d4d2035b75d97e403260a214f2509e7e
andreap <andreasofiapais@gmail.com>
2025-05-13 20:02:43 +0200

    Cambios Finales Tareas

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
 
         // Elemento que es la nueva nota dentro de una lista
         const nuevaNota = $("<li class='tarea'></li>");
-        nuevaNota.append(`<div class="check"></div>`);
-        $(".check").click(function(){
-            $(".check").toggleClass("completado");
-            $(".check").removeClass("check").addClass("completado");
-        })
+    
         nuevaNota.append(`<h3 class="titulo_tarea">${titulo}</h3>`);
         nuevaNota.append(`<p class="descrip_tarea">${descripcion}</p>`);
-        nuevaNota.append(`<div class="borrar"></div>`)
+        nuevaNota.append(`<div class="botones">
+            <div class="borrar"></div>
+            <div class="check"></div>
+            </div>`);
         
+
         // Agregar a la lista pero las siguientes aparacen arriba
         $("#lista_tareas").prepend(nuevaNota);
 
@@ -23,18 +23,32 @@ $(document).ready(function(){
         $(".descripcion").val("");
     });
 
+    $("#lista_tareas").on("click",".borrar",function(){
+        $(this).closest(".tarea").remove();
+    });
+
+    $("#lista_tareas").on("click",".check",function(){
+        $(this,".check").toggleClass("completado");
+        if($(".check").hasClass("completado")){
+
+        }
+    });
+
+
     $(".modo").click(function(){
         $("html").toggleClass("cambio");
-        if($("html").hasClass("cambio")){
+        /*if($("html").hasClass("cambio")){
             $(".oscuro").removeClass("oscuro").addClass("modo_claro");
             $(".modo").text("Modo claro");        
         }
         else{
             $(".oscuro").toggleClass("oscuro");
             $(".modo").text("Modo oscuro"); 
-        }
+        }*/
         
-    })
+    });
+
+    
 
 
 })
